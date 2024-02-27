6163c6355fb88e0efe1f65fc37ac92f8e2557e82
Geo <george.nogovitsin@gmail.com>
2024-02-27 20:30:37 +0300

    #7 made Drawer class

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,3 +1,47 @@
 'use strict';
 
 import './style.css';
+import Drawer from '@/battleships/drawer.js'
+
+const config = {
+  0: {
+    class: 'cell sea',
+    handle: () => void{},
+  },
+  1: {
+    class: 'cell sea-hit',
+  },
+  2: {
+    class: 'cell ship',
+  },
+  3: {
+    class: 'cell ship-hit',
+  },
+};
+
+
+const map = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+function handle(e) {
+  console.log(e)
+}
+
+let userField = document.getElementById("user-field");
+let rivalField = document.getElementById("rival-field");
+
+const userDrawer = new Drawer(userField, config);
+let rivalDrawer = new Drawer(rivalField, config, handle);
+
+userDrawer.draw(map);
+rivalDrawer.draw(map);
