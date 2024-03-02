11e32a94d1de3535d874509edcc8fbbc8e4763c2
mireal <moveloader@yandex.ru>
2024-03-02 20:41:13 +0300

    #16 alternative attack function with cell and ship explosions

--- src/game/explodeship.js
+++ src/game/explodeship.js
@@ -1,16 +1,16 @@
 'use strict';
 
 function explodeShip(map, ship) {
-  for (const cell of ship.cells) {
-    const x_max = Math.min(9, cell.x + 1);
-    const y_max = Math.min(9, cell.y + 1);
+  const indexes = [-1, 0, 1];
 
-    for (let y = Math.max(0, cell.y - 1); y <= y_max; y++) {
-      for (let x = Math.max(0, cell.x - 1); x <= x_max; x++) {
-        const cell = map.value[y][x];
-        if (cell.type == 'sea') {
-          cell.type = 'sea-hit';
-          cell.isHit = true;
+  for (const cell of ship.cells) {
+    for (let dy of indexes) {
+      for (let dx of indexes) {
+        let y = cell.y + dy;
+        let x = cell.x + dx;
+
+        if (0 <= y && 0 <= x && y < 10 && x < 10 && map[y][x].type == 'sea') {
+          map[y][x].isHit = true;
         }
       }
     }
