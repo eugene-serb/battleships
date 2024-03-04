f1e6651aee8ab9c203ebd6eb1f599fa4a4c0cfa7
mireal <moveloader@yandex.ru>
2024-03-04 22:08:05 +0300

    #21 #29 Endgame check

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,10 +4,26 @@ import './style.css';
 
 import Gameloop from '../../game/gameloop';
 
+const config = {
+  0: {
+    class: 'cell sea',
+  },
+  1: {
+    class: 'cell sea-hit',
+  },
+  2: {
+    class: 'cell ship',
+  },
+  3: {
+    class: 'cell ship-hit',
+  },
+};
+
 const userField = document.getElementById('user-field');
 const rivalField = document.getElementById('rival-field');
 const restartButton = document.getElementById('restart');
+const winText = document.getElementById('win-text');
 
-const gameloop = new Gameloop(userField, rivalField, restartButton);
+const gameloop = new Gameloop(userField, rivalField, restartButton, winText, config);
 
 gameloop.init();
